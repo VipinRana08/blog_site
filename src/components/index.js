d551207481d1e6e32445e73a376c670b01137f22
Vipin Singh Rana <vipinrana08.vr@gmail.com>
2025-02-06 20:48:09 +0530

    Add and configure Header, Footer, Button, Input, Logo, LogoutBtn, Container components

--- src/components/index.js
+++ src/components/index.js
@@ -1,6 +1,11 @@
 import Header from "./Header/Header"
 import Footer from "./Footer/Footer"
+import Input from "./input"
+import Logo from "./Logo"
+import LogoutBtn from "./Header/LogoutBtn"
+import Container from "./container/"
 
 export {
-    Header, Footer
+    Header, Footer, Input,
+    Logo, LogoutBtn, Container,
 }
