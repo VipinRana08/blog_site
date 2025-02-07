05595e8deeea739cccb4939bc45bb6912691c2cc
Vipin Singh Rana <vipinrana08.vr@gmail.com>
2025-02-07 19:46:05 +0530

    Add Login Signup AuthLayout components

--- src/components/index.js
+++ src/components/index.js
@@ -3,9 +3,17 @@ import Footer from "./Footer/Footer"
 import Input from "./input"
 import Logo from "./Logo"
 import LogoutBtn from "./Header/LogoutBtn"
-import Container from "./container/"
+import Container from "./container/Container"
+import AuthLayout from "./AuthLayout"
+import Login from "./Login"
+import Select from "./Select"
+import PostCard from "./PostCard"
+import Button from "./Button"
+import Input from "./input"
 
 export {
     Header, Footer, Input,
     Logo, LogoutBtn, Container,
+    AuthLayout, Login, Select, 
+    PostCard, Button, Input
 }
